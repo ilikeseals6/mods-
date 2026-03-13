4416f107664a295ed45101812ea5fdab5080d57f
ilikeseals6 <jukimpfb@necedahschools.org>
2026-03-13 13:53:42 -0500

    Update curse_and_sin.js with proper Sandboxels element definitions for curse, sin, wither, and tainted_water

--- curse_and_sin.js
+++ curse_and_sin.js
@@ -1,5 +1,42 @@
-// Sandboxels format
+// Sandboxels Element Definitions
 
-// Your code implementation here.
+const elements = {
+  "curse": {
+    "type": "element",
+    "properties": {
+      "color": "#000000",
+      "effect": "cursed",
+      "duration": 300,
+      "intensity": 1
+    }
+  },
+  "sin": {
+    "type": "element",
+    "properties": {
+      "color": "#800000",
+      "effect": "sinful",
+      "duration": 400,
+      "intensity": 2
+    }
+  },
+  "wither": {
+    "type": "element",
+    "properties": {
+      "color": "#4B0082",
+      "effect": "withering",
+      "duration": 200,
+      "intensity": 3
+    }
+  },
+  "tainted_water": {
+    "type": "element",
+    "properties": {
+      "color": "#4682B4",
+      "effect": "tainted",
+      "duration": 600,
+      "intensity": 1
+    }
+  }
+};
 
-console.log('This file is now in Sandboxels format.');
+export default elements;
